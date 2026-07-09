23895f2e0e39c67edfe789c4f2d67ac009d42025
hossein shoshtary <hossein.shoshtary46@gmail.com>
2026-07-09 20:46:43 +0330

    do sam work on admin panel

--- Multi_shop_2/settings.py
+++ Multi_shop_2/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # my apps
-    "Home",
+    "Home.apps.HomeConfig",
+    "account.apps.AccountConfig",
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "account.User"
