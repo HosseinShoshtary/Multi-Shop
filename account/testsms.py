c24e69326087ee8e04860db6f75af9a7f596278c
hossein shoshtary <hossein.shoshtary46@gmail.com>
2026-07-13 01:26:44 +0330

    send massage for user with ghasedak

---
+++ account/testsms.py
@@ -0,0 +1,8 @@
+# import ghasedak_sms
+#
+# sms = ghasedak_sms.Ghasedak("A place where you can put the API Key")
+#
+# response = sms_api.get_otp_parameters(
+#     ghasedak_sms.GetOtpParametersInput(template_name='YourTemplateName')
+# )
+# print(response)
