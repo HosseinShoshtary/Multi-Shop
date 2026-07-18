353f0cb6902d7ba20d5b8c228c8dfbd2ecad237a
hossein shoshtary <hossein.shoshtary46@gmail.com>
2026-07-18 16:08:51 +0330

    now you can delete items from cart

--- cart/views.py
+++ cart/views.py
@@ -13,8 +13,15 @@ class CartDetailView(View):
 class CartAddView(View):
     def post(self, request, pk):
         product = get_object_or_404(Product, id=pk)
-        size, color, quantity = request.POST.get("size"), request.POST.get("color"), request.POST.get("quantity")
+        size, color, quantity = request.POST.get("size", "empty"), request.POST.get("color", "empty"), request.POST.get("quantity")
         print(size,color,quantity)
         cart = Cart(request)
         cart.add(product, quantity, color, size)
+        return redirect("cart:cart_detail")
+
+
+class CartDeleteView(View):
+    def get(self, request, id):
+        cart = Cart(request)
+        cart.delete(id)
         return redirect("cart:cart_detail")
